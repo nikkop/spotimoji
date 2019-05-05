90b9ae218faece1c2971db6e1c9643ac91b142c6
Nikola Pantic <nikolaprivat@nikolambp.local>
2019-05-05 19:24:25 +0200

    Add styling for visual awesomeness

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import emojis from './emojis'
 import style from './style.module.css'
 
-const { access_token, token_type, expires_in, state } = window.location.hash
+const { access_token, token_type } = window.location.hash
   .slice(1)
   .split('&')
   .map(hash => hash.split('='))
@@ -17,6 +17,10 @@ const getCurrentlyPlaying = async () =>
 
 const getAudioFeatures = async () => {
   const currentTrack = await getCurrentlyPlaying()
+
+  const albumImage = currentTrack.item.album.images[0]
+  document.body.style.backgroundImage = `url(${albumImage.url})`
+
   if (currentTrack.is_playing) {
     const audioFeatures = await fetchData(
       `audio-features/${currentTrack.item.id}`
@@ -26,46 +30,47 @@ const getAudioFeatures = async () => {
 }
 const showFeatures = async () => {
   const audioFeatures = await getAudioFeatures()
-  console.log(audioFeatures)
-  const featureKeys = [
-    'acousticness',
-    'danceability',
-    'energy',
-    'instrumentalness',
-    'valence',
-  ]
-  const features = Object.entries(audioFeatures)
-    .filter(([feature]) => featureKeys.includes(feature))
-    .map(([feature, value]) => ({
-      feature,
-      emoji: emojis[feature][Math.round(value)],
-      value,
-    }))
+  if (audioFeatures) {
+    const featureKeys = [
+      'acousticness',
+      'danceability',
+      'energy',
+      'instrumentalness',
+      'valence',
+    ]
+    const features = Object.entries(audioFeatures)
+      .filter(([feature]) => featureKeys.includes(feature))
+      .map(([feature, value]) => ({
+        feature,
+        emoji: emojis[feature][Math.round(value)],
+        value,
+      }))
 
-  const container = document.createElement('div')
-  container.classList.add(style.container)
-  document.body.appendChild(container)
+    const container = document.createElement('div')
+    container.classList.add(style.container)
+    document.body.appendChild(container)
 
-  const featureElements = ['feature', 'emoji', 'value'].map(type => {
-    const el = document.createElement('span')
-    el.classList.add(type)
-    return el
-  })
-  container.append(...featureElements)
+    const featureElements = ['feature', 'emoji', 'value'].map(type => {
+      const el = document.createElement('span')
+      el.classList.add(style[type])
+      return el
+    })
+    container.append(...featureElements)
 
-  const renderFeature = ({ feature, value, emoji }) => {
-    featureElements[0].innerHTML = feature
-    featureElements[1].innerHTML = emoji
-    featureElements[2].innerHTML = value
+    const renderFeature = ({ feature, value, emoji }) => {
+      featureElements[0].innerHTML = feature
+      featureElements[1].innerHTML = emoji
+      featureElements[2].innerHTML = `${Math.round(value * 100)}%`
+    }
+
+    let i = 0
+    setInterval(() => {
+      const n = features.length
+      const feature = features[((i % n) + n) % n]
+      renderFeature(feature, container)
+      i++
+    }, 1500)
   }
-
-  let i = 0
-  setInterval(() => {
-    const n = features.length
-    const feature = features[((i % n) + n) % n]
-    renderFeature(feature, container)
-    i++
-  }, 1500)
 }
 
 showFeatures()
